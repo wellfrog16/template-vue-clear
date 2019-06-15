f3a8f1093d075d04e72abb4098337bea7c0ea666
wellfrog <wellfrog16@gmail.com>
2019-06-15 11:48:25 +0800

    更新校验

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -1,6 +1,13 @@
 const rules = {
-    // 非空
-    noEmpty({ key, message }) {
+    /**
+     * 非空校验
+     *
+     * @param {String} key 校验的字段
+     * @param {JSON} [options={}] { message: 提示信息（不能为空） }
+     * @returns
+     */
+    noEmpty(key, options = {}) {
+        const { message } = Object.assign({ message: '不能为空' }, options);
         const r = {};
         r[key] = [
             { required: true, message, trigger: 'change' },
@@ -8,11 +15,66 @@ const rules = {
         return r;
     },
 
-    // 自带类型校验
-    check(args) {
-        const params = Object.assign({ required: true, trigger: 'blur' }, args);
+    /**
+     * 自带类型校验
+     *
+     * @param {String} key 校验的字段
+     * @param {JSON} [options={}]
+     * @returns
+     */
+    check(key, options = {}) {
+        const params = Object.assign({ required: true, trigger: 'blur' }, options);
         const r = {};
-        r[params.key] = [{ ...params }];
+        r[key] = [{ ...params }];
+        return r;
+    },
+
+    /**
+     * 校验字符串
+     *
+     * @param {*} key
+     * @param {*} [options={}] { ?allowEmpty: 允许为空(true), ?allowSpecial: 允许特殊字符(true) }
+     * @returns
+     */
+    checkString(key, options = {}) {
+        const params = Object.assign({ name: '', allowEmpty: true, allowSpecial: true }, options);
+        const { name, emptyMessage } = params;
+        const r = {};
+        const rule = [];
+
+        // 是否允许特殊字符，待扩展
+        if (!params.allowSpecial) {
+            rule.push({ pattern: /^[\u4e00-\u9fa5|a-z|A-Z]+$/, message: `${name}只可中文/英文/拼音，不可特殊符号，数字`, trigger: 'change' });
+        }
+
+        // 是否允许为空
+        if (!params.allowEmpty) {
+            rule.push({ required: true, message: (emptyMessage || `请输入${name}`), trigger: 'change' });
+        }
+
+        r[key] = rule;
+        return r;
+    },
+
+    /**
+     * 手机号校验
+     *
+     * @param {String} key 校验的字段
+     * @param {JSON} [options={}] { ?allowEmpty: 允许为空(true) }
+     * @returns
+     */
+    isPhoneNumber(key, options = {}) {
+        const params = Object.assign({ allowEmpty: true }, options);
+        const r = {};
+        const rule = [
+            { pattern: /^1\d{10}$/, message: '请输入正确的手机号', trigger: 'change' },
+        ];
+
+        // 是否允许为空
+        if (!params.allowEmpty) {
+            rule.push({ required: true, message: '请输入手机号', trigger: 'change' });
+        }
+        r[key] = rule;
         return r;
     },
 };
